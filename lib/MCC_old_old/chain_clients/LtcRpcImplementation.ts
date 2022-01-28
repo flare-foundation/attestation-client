f15b887fb686ac4b17249979a37de22fcffbb7cb
Luka Avbreht <luka.avbreht@aflabs.si>
2022-01-28 17:06:53 +0100

    removed MCC definition

--- lib/MCC_old_old/chain_clients/LtcRpcImplementation.ts
+++
@@ -1,8 +0,0 @@
-import { UtxoMccCreate, UtxoRpcInterface } from "../types";
-import { UtxoCore } from "./UtxoCore";
-
-export class LTCImplementation extends UtxoCore implements UtxoRpcInterface {
-  constructor(options: UtxoMccCreate) {
-    super(options.url, options.username, options.password, options.rateLimitOptions || {}, options.inRegTest || false);
-  }
-}
--- lib/MCC_old_old/chain_clients/dev_docs/XRP.md
+++
@@ -1,7 +0,0 @@
-## Ripple rpc docs
-
-https://xrpl.org/tx.html
-https://xrpl.org/get-started-using-http-websocket-apis.html
-
-health:
-https://xrpl.org/health-check.html

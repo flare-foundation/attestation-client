df16ded9595817b6c5757b600cdb7483e9a804cb
Luka Avbreht <luka.avbreht@aflabs.si>
2022-01-28 16:29:47 +0100

    removed unused imports

--- test/utils/test-utils.ts
+++ test/utils/test-utils.ts
@@ -1,6 +1,5 @@
 import { expectEvent } from "@openzeppelin/test-helpers";
 import { ChainType, IUtxoBlockRes, prefix0x, RPCInterface, toBN } from "flare-mcc";
-import { prettyPrintObject } from "../../lib/utils/utils";
 import {
   attReqToTransactionAttestationRequest,
   extractAttEvents,

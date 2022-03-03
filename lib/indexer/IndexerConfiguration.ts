ea749cb6a95b4de7e47366e87df28e94bf8f6589
Luka Avbreht <luka.avbreht@aflabs.si>
2022-03-03 13:03:42 +0100

    updated config files

--- lib/indexer/IndexerConfiguration.ts
+++ lib/indexer/IndexerConfiguration.ts
@@ -1,17 +1,10 @@
+import { MccCreate, RateLimitOptions } from "flare-mcc";
+
 export class IndexerClientChain {
   public name!: string;
-  public url!: string;
 
-  public username!: string;
-  public password!: string;
-
-  public maxRequestsPerSecond: number = 2;
-  public maxProcessingTransactions: number = 10;
-
-  public clientTimeout: number = 3000;
-  public clientRetries: number = 3;
-
-  public maxFailedRetry: number = 1;
+  public mccCreate: MccCreate
+  public rateLimitOptions: RateLimitOptions;
   
   public confirmationsCollect: number = 6;
 }

5af51e032e2a592f570c426abb055fa06f04a840
Luka Avbreht <luka.avbreht@aflabs.si>
2022-03-07 09:55:15 +0100

    updated read transacation

--- lib/indexer/chain-collector-helpers/readTransaction.ts
+++ lib/indexer/chain-collector-helpers/readTransaction.ts
@@ -14,40 +14,27 @@ export async function getFullTransactionUtxo(client: CachedMccClient<any,any>, b
   //   return null;
   // }
   // let response: IUtxoGetFullTransactionRes = { vinouts: [], ...res.data };
-  blockTransaction.fullData = { vinouts: [], ...blockTransaction.fullData };
-  let vinPromises = blockTransaction.data.vin.map(async (vin: IUtxoVinTransaction) => {
-    if (vin.txid) {
-      // the in-transactions are prepended to queue in order to process them earlier
-      let tx = (await processor.call(() => client.getTransaction(vin.txid), true)) as UtxoTransaction;
-      const inVout = vin.vout!;
-      if (tx.data.vout[inVout].n != inVout) {
-        throw Error("Vin and vout transaction miss match");
+  if(blockTransaction.reference.length > 0){
+    blockTransaction.fullData = { vinouts: [], ...blockTransaction.fullData };
+    let vinPromises = blockTransaction.data.vin.map(async (vin: IUtxoVinTransaction) => {
+      if (vin.txid) {
+        // the in-transactions are prepended to queue in order to process them earlier
+        let tx = (await processor.call(() => client.getTransaction(vin.txid), true)) as UtxoTransaction;
+        const inVout = vin.vout!;
+        if (tx.data.vout[inVout].n != inVout) {
+          throw Error("Vin and vout transaction miss match");
+        }
+        return tx.data.vout[inVout];
+      } else {
+        return { coinbase: vin.coinbase } as IUtxoCoinbase;
       }
-      return tx.data.vout[inVout];
-    } else {
-      return { coinbase: vin.coinbase } as IUtxoCoinbase;
-    }
-  });
-  let vinInputs = await Promise.all(vinPromises);
-  blockTransaction.fullData.vinouts = vinInputs;
-  processor.markTopLevelJobDone();
-  return blockTransaction;
+    });
+    let vinInputs = await Promise.all(vinPromises);
+    blockTransaction.fullData.vinouts = vinInputs;
+    processor.markTopLevelJobDone();
+    return blockTransaction;
+  }
+  else {
+    return blockTransaction
+  }
 }
-
-
-
-// export async function readTransactionAlgo(client: ReadRpcInterface, txHash: string): Promise<IAlgoGetFullTransactionRes> {
-//   return (await client.getTransaction(txHash)) as IAlgoGetFullTransactionRes;
-// }
-
-// export async function readTransactionUtxo(client: ReadRpcInterface, txHash: string): Promise<IUtxoGetFullTransactionRes> {
-//   return (await client.getTransaction(txHash)) as IUtxoGetFullTransactionRes;
-// }
-
-// export async function readTransactionXrp(client: ReadRpcInterface, txHash: string): Promise<IXrpGetFullTransactionRes> {
-//   return (await client.getTransaction(txHash)) as IXrpGetFullTransactionRes;
-// }
-
-// export async function readTransactionDefault(client: ReadRpcInterface, txHash: string): Promise<any> {
-//   return await client.getTransaction(txHash);
-// }

cfc84cc6aef98c2c267c7e4c5d4f3264efb2c326
evi1m3 <david@altumvi.com>
2022-04-05 20:16:11 +0200

    ...

--- lib/attester/AttesterClient.ts
+++ lib/attester/AttesterClient.ts
@@ -234,14 +234,14 @@ export class AttesterClient {
 }
 
 
-process.on('SIGINT', () => {
-  console.log('Received SIGINT. Press Control-D to exit.');
-});
+// process.on('SIGINT', () => {
+//   console.log('Received SIGINT. Press Control-D to exit.');
+// });
 
-// Using a single function to handle multiple signals
-function handle(signal) {
-  console.log(`Received ${signal}`);
-}
+// // Using a single function to handle multiple signals
+// function handle(signal) {
+//   console.log(`Received ${signal}`);
+// }
 
-process.on('SIGINT', handle);
-process.on('SIGTERM', handle);
+// process.on('SIGINT', handle);
+// process.on('SIGTERM', handle);

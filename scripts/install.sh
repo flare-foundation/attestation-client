c4239a8a0a0eda9b61f4ca9bb15becf9ed38247e
David Pangerl <david@altumvi.com>
2022-05-12 08:40:46 +0200

    installation script

---
+++ scripts/install.sh
@@ -0,0 +1,28 @@
+export RED='\033[0;31m'
+export GREEN='\033[0;32m'
+export NC='\033[0m' # No Color
+export REDBOLD="${RED}$(tput bold)"
+export GREENBOLD="${GREEN}$(tput bold)"
+export NCNORMAL="${NC}$(tput sgr0)"
+
+# copy services
+echo -e "${REDBOLD}[1] ${GREENBOLD}Copying services...${NC}"
+mkdir -p ~/.config/systemd/user
+cp ./scripts/templates/*.service ~/.config/systemd/user
+
+# enable services
+echo -e "${REDBOLD}[2] ${GREENBOLD}Installing services...${NC}"
+systemctl --user daemon-reload
+
+systemctl --user enable indexer-xrp.service
+systemctl --user enable indexer-btc.service
+systemctl --user enable indexer-ltc.service
+systemctl --user enable indexer-algo.service
+systemctl --user enable indexer-doge.service
+
+systemctl --user enable songbird-attester-client.service
+systemctl --user enable songbird-backend.service
+
+systemctl --user enable attester-alerts
+
+bash ./scripts/deploy-all.sh

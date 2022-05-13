f42155a9768279dc3dcd2eaaa0c40cfe91f5fb09
David Pangerl <david@altumvi.com>
2022-05-13 12:32:25 +0200

    installation

--- scripts/deploy-all.sh
+++ scripts/deploy-all.sh
@@ -6,14 +6,14 @@ export GREENBOLD="${GREEN}$(tput bold)"
 export NCNORMAL="${NC}$(tput sgr0)"
 
 # install services
-echo -e "${REDBOLD}[3] ${GREENBOLD}Installing Indexer...${NC}"
+echo -e "${REDBOLD}[4.1] ${GREENBOLD}Installing Indexer...${NC}"
 bash ./scripts/deploy-indexer
 
-echo -e "${REDBOLD}[4] ${GREENBOLD}Installing Alerts...${NC}"
-#bash ./scripts/deploy-alerts
+echo -e "${REDBOLD}[4.2] ${GREENBOLD}Installing Alerts...${NC}"
+bash ./scripts/deploy-alerts
 
-echo -e "${REDBOLD}[5] ${GREENBOLD}Installing Songbird Attester Client...${NC}"
-#bash ./scripts/deploy-songbird-attester
+echo -e "${REDBOLD}[4.3] ${GREENBOLD}Installing Songbird Attester Client...${NC}"
+bash ./scripts/deploy-songbird-attester
 
-echo -e "${REDBOLD}[6] ${GREENBOLD}Installing Songbird Backend...${NC}"
-#bash ./scripts/deploy-songbird-backend
+echo -e "${REDBOLD}[4.4] ${GREENBOLD}Installing Songbird Backend...${NC}"
+bash ./scripts/deploy-songbird-backend

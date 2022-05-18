c43721da671b0dfae230f3eb9c4c7fddd0671aba
alenabelium <33514602+alenabelium@users.noreply.github.com>
2022-05-18 16:44:33 +0200

    adding performance settings to status on backend

--- lib/webserver/controllers/proofController.ts
+++ lib/webserver/controllers/proofController.ts
@@ -1,12 +1,9 @@
-import { Controller, Get, Path, Route, Tags, Request } from "tsoa";
+import { Controller, Get, Path, Route, Tags } from "tsoa";
 import { Factory, Inject, Singleton } from "typescript-ioc";
-import { DBVotingRoundResult } from "../../entity/attester/dbVotingRoundResult";
-import { ServiceStatus } from "../dto/ServiceStatus";
 import { SystemStatus } from "../dto/SystemStatus";
 import { VotingRoundResult } from "../dto/VotingRoundResult";
 import { ProofEngine } from "../engines/proofEngine";
 import { ApiResponse, handleApiResponse } from "../models/ApiResponse";
-import * as express from "express";
 
 @Tags('Proof')
 @Route("api/proof")

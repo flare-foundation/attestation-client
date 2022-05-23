0ec017492317601d8c5d45de264cc3028cb83857
David Pangerl <david@altumvi.com>
2022-05-23 10:38:47 +0200

    admin refactoring

--- lib/admin/admin.ts
+++ lib/admin/admin.ts
@@ -4,50 +4,53 @@ import { DotEnvExt } from "../utils/DotEnvExt";
 import { getGlobalLogger } from "../utils/logger";
 import { EServiceStatus } from "../utils/serviced";
 import { sleepms } from "../utils/utils";
-import { Menu, MenuItemCommand, MenuItemLog, MenuItemService } from "./menu";
+import { Menu } from "./menu";
+import { MenuItemCommand } from "./menuItemCommand";
+import { MenuItemLog } from "./menuItemLog";
+import { MenuItemService } from "./menuItemService";
 
 async function admin() {
     const menu = new Menu();
 
     menu.addCommand("Update", "git pull & bash ./scripts/compile.sh");
     menu.addSubmenu("Deploy").
-        addCommand("^RAll", "bash ./scripts/deploy-all").parent.
-        addCommand("Indexers", "bash ./scripts/deploy-indexer").parent.
-        addCommand("Alerts", "bash ./scripts/deploy-alerts").parent.
-        addCommand("Coston Attestation Client", "bash ./scripts/deploy-coston-attester").parent.
-        addCommand("Coston backend", "bash ./scripts/deploy-coston-backend").parent.
-        addCommand("Songbird Attestation Client", "bash ./scripts/deploy-songbird-attester").parent.
+        addCommand("^RAll", "bash ./scripts/deploy-all").parent().
+        addCommand("Indexers", "bash ./scripts/deploy-indexer").parent().
+        addCommand("Alerts", "bash ./scripts/deploy-alerts").parent().
+        addCommand("Coston Attestation Client", "bash ./scripts/deploy-coston-attester").parent().
+        addCommand("Coston backend", "bash ./scripts/deploy-coston-backend").parent().
+        addCommand("Songbird Attestation Client", "bash ./scripts/deploy-songbird-attester").parent().
         addCommand("Songbird backend", "bash ./scripts/deplay-songbird-backend");
 
     menu.addSubmenu("Services").
-        addCommand("^RRestart all", "bash ./scripts/services-restart-all").parent.
-        addCommand("^RStop all", "bash ./scripts/services-stop-all").parent.
+        addCommand("^RRestart all", "bash ./scripts/services-restart-all").parent().
+        addCommand("^RStop all", "bash ./scripts/services-stop-all").parent().
         addSubmenu("Indexer").
-            addService("ALGO", "indexer-algo").parent.
-            addService("BTC", "indexer-btc").parent.
-            addService("DOGE", "indexer-doge").parent.
-            addService("LTC", "indexer-ltc").parent.
-            addService("XRP", "indexer-xrp").parent.
-        parent.
-        addService("Alerts", "attester-alerts").parent.
-        addService("Coston Attestation Client", "coston-attester-client").parent.
-        addService("Coston backend", "coston-backend").parent.
-        addService("Songbird Attestation Client", "songbird-attester-client").parent.
+            addService("ALGO", "indexer-algo").parent().
+            addService("BTC", "indexer-btc").parent().
+            addService("DOGE", "indexer-doge").parent().
+            addService("LTC", "indexer-ltc").parent().
+            addService("XRP", "indexer-xrp").parent().
+        parent().
+        addService("Alerts", "attester-alerts").parent().
+        addService("Coston Attestation Client", "coston-attester-client").parent().
+        addService("Coston backend", "coston-backend").parent().
+        addService("Songbird Attestation Client", "songbird-attester-client").parent().
         addService("Songbird backend", "songbird-backend").parent;
 
     menu.addSubmenu("Show log").
         addSubmenu("Indexer").
-            addLog("ALGO", "../global/indexer/logs/attester-ALGO.log").parent.
-            addLog("BTC", "../global/indexer/logs/attester-BTC.log").parent.
-            addLog("DOGE", "../global/indexer/logs/attester-DOGE.log").parent.
-            addLog("LTC", "../global/indexer/logs/attester-LTC.log").parent.
-            addLog("XRP", "../global/indexer/logs/attester-XRP.log").parent.
-        parent.
-        addLog("app", "logs/attester-global.log").parent.
-        addLog("Alerts", "../global/alerts/logs/attester-global.log").parent.
-        addLog("Coston Attestation Client", "../coston/attester-client/logs/attester-global.log").parent.
-        addLog("Coston backend", "../coston/backend/logs/attester-global.log").parent.
-        addLog("Songbird Attestation Client", "../songbird/attester-client/logs/attester-global.log").parent.
+            addLog("ALGO", "../global/indexer/logs/attester-ALGO.log").parent().
+            addLog("BTC", "../global/indexer/logs/attester-BTC.log").parent().
+            addLog("DOGE", "../global/indexer/logs/attester-DOGE.log").parent().
+            addLog("LTC", "../global/indexer/logs/attester-LTC.log").parent().
+            addLog("XRP", "../global/indexer/logs/attester-XRP.log").parent().
+        parent().
+        addLog("app", "logs/attester-global.log").parent().
+        addLog("Alerts", "../global/alerts/logs/attester-global.log").parent().
+        addLog("Coston Attestation Client", "../coston/attester-client/logs/attester-global.log").parent().
+        addLog("Coston backend", "../coston/backend/logs/attester-global.log").parent().
+        addLog("Songbird Attestation Client", "../songbird/attester-client/logs/attester-global.log").parent().
         addLog("Songbird backend", "../songbird/backend/logs/attester-global.log").parent;
 
     //await menu.run();

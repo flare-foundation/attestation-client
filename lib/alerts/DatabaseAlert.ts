b75c1fd258c37f50a7345d4a318391698c330367
David Pangerl <david@altumvi.com>
2022-06-15 08:32:15 +0200

    monitor perf update

--- lib/alerts/DatabaseAlert.ts
+++ lib/alerts/DatabaseAlert.ts
@@ -39,7 +39,7 @@ export class DatabaseAlert extends AlertBase {
 
                 if (user.user === "root" || user.user === "event_scheduler" || user.user === "processReader") continue;
 
-                resArray.push( new PerformanceInfo( `mysql.${user.user}` , "time" , user.time , "ms" ,`${user.conn} connection(s)` ) );
+                resArray.push(new PerformanceInfo(`mysql.${user.user}`, "time", user.time, "ms", `${user.conn} connection(s)`));
             }
         }
 
@@ -49,24 +49,25 @@ export class DatabaseAlert extends AlertBase {
         const now = getTimeMilli();
         const cpus = os.cpus();
 
+        // update memory information
         // check free memory
         //const freeMemory = round( os.freemem() / (1024*1024) , 1 );
         // check the total memory
-        const totalMemory = round( os.totalmem() / (1024*1024) , 1 );
+        const totalMemory = round(os.totalmem() / (1024 * 1024), 1);
 
         const memInfo = fs.readFileSync('/proc/meminfo', 'utf8');
-        const availableMemory = round( Number(/MemAvailable:[ ]+(\d+)/.exec(memInfo)[1]) / 1024 , 1 );
-        const freeMemory = round( Number(/MemFree:[ ]+(\d+)/.exec(memInfo)[1]) / 1024 , 1 );
+        const availableMemory = round(Number(/MemAvailable:[ ]+(\d+)/.exec(memInfo)[1]) / 1024, 1);
+        const freeMemory = round(Number(/MemFree:[ ]+(\d+)/.exec(memInfo)[1]) / 1024, 1);
 
-        resArray.push( new PerformanceInfo(`system.memory`, "total", totalMemory , "MB" ) );
+        resArray.push(new PerformanceInfo(`system.memory`, "total", totalMemory, "MB"));
 
-        resArray.push( new PerformanceInfo(`system.memory`, "free", freeMemory, "MB" , `${round(freeMemory*100/totalMemory,1)}% free` ) );
+        resArray.push(new PerformanceInfo(`system.memory`, "free", freeMemory, "MB", `${round(freeMemory * 100 / totalMemory, 1)}% free`));
 
-        resArray.push( new PerformanceInfo(`system.memory`, "available", availableMemory, "MB", `${round(availableMemory*100/totalMemory,1)}% available` ) );
+        resArray.push(new PerformanceInfo(`system.memory`, "available", availableMemory, "MB", `${round(availableMemory * 100 / totalMemory, 1)}% available`));
 
+        // update disk information 
         const nodeDiskInfo = require('node-disk-info');
-        // async way
-        if( now > this.diskCheckTime ) {
+        if (now > this.diskCheckTime) {
             nodeDiskInfo.getDiskInfo()
                 .then(disks => {
                     this.disks = disks;
@@ -74,16 +75,20 @@ export class DatabaseAlert extends AlertBase {
                     // check once per 10 minutes
                     this.diskCheckTime = now + 60 * 10 * 1000;
                 })
-                .catch(error => { logException( error , `nodeDiskInfo` );});        
-            }
+                .catch(error => { logException(error, `nodeDiskInfo`); });
+        }
 
-        if( this.disks ) {
-            for( let disk of this.disks ) {
-                if( disk.mounted!=="/" ) continue;
+        if (this.disks) {
+            for (let disk of this.disks) {
+                if (disk.mounted !== "/") continue;
 
-                resArray.push( new PerformanceInfo( `system.disk.available`, disk.filesystem, 
-                    round( disk.available / (1024 * 1024 ) , 3 ), "GB" ,
-                    `${round(disk.available*100/(disk.available+disk.used),1)}% of ${round((disk.available+disk.used)/(1024*1024),3)} GB available` ) );
+                const total = disk.available + disk.used;
+
+                resArray.push(new PerformanceInfo(`system.disk.${disk.filesystem}`, `total`, round(total / (1024 * 1024), 3), "GB" ) );
+
+                resArray.push(new PerformanceInfo(`system.disk.${disk.filesystem}`, `available`, round(disk.available / (1024 * 1024), 3), "GB", `${round(disk.available * 100 / total, 1)}% available`));
+
+                resArray.push(new PerformanceInfo(`system.disk.${disk.filesystem}`, `used`, round(disk.used / (1024 * 1024), 3), "GB", `${round(disk.used * 100 / total, 1)}% used`));
             }
         }
 
@@ -92,16 +97,15 @@ export class DatabaseAlert extends AlertBase {
         for (let i = 0; i < cpus.length; i++) {
             const cpu = cpus[i];
 
-            //idle+=cpu.times.idle;
             used += cpu.times.sys + cpu.times.user;
         }
 
         used /= cpus.length;
 
-        resArray.push( new PerformanceInfo( `system.cpu`, "count" , cpus.length, "" , cpus[0].model ) );
+        resArray.push(new PerformanceInfo(`system.cpu`, "count", cpus.length, "", cpus[0].model));
 
         if (this.cpuTime > 0) {
-            resArray.push( new PerformanceInfo( `system.cpu` , "utilization" , round((used - this.cpuUsed) * 100 / (now - this.cpuTime), 1) , "%" ) );
+            resArray.push(new PerformanceInfo(`system.cpu`, "utilization", round((used - this.cpuUsed) * 100 / (now - this.cpuTime), 1), "%"));
         }
 
         this.cpuTime = now;

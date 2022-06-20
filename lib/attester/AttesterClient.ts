06a3a04d32f1336a742a4248706fc9d62aa07961
alenabelium <33514602+alenabelium@users.noreply.github.com>
2022-06-20 13:52:19 +0200

    tracer option change

--- lib/attester/AttesterClient.ts
+++ lib/attester/AttesterClient.ts
@@ -65,7 +65,7 @@ export class AttesterClient {
     try {
       const version = "1002";
 
-      traceManager.displayTrace = true;
+      traceManager.displayRuntimeTrace = true;
 
       this.logger.title(`starting Flare Attester Client v${version}`);
 

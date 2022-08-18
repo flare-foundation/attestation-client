e8c18c330ed743680568d087840bba8a47a45aed
evi1m3 <david.pangerl@flare.network>
2022-08-18 16:03:49 +0200

    remote database server install

--- configs/.install/templates/update.sql
+++ configs/.install/templates/update.sql
@@ -1,12 +1,12 @@
-ALTER USER '$(IndexerWriterUsername)'@'localhost' IDENTIFIED BY '$(IndexerWriterPassword)';
+ALTER USER '$(IndexerWriterUsername)'@'$(DatabaseWriteAccessSource)' IDENTIFIED BY '$(IndexerWriterPassword)';
 
 ALTER USER '$(IndexerReaderUsername)'@'%' IDENTIFIED BY '$(IndexerReaderPassword)';
 
-ALTER USER '$(AttesterWriterUsernameCoston)'@'localhost' IDENTIFIED BY '$(AttesterWriterPasswordCoston)';
+ALTER USER '$(AttesterWriterUsernameCoston)'@'$(DatabaseWriteAccessSource)' IDENTIFIED BY '$(AttesterWriterPasswordCoston)';
 
 ALTER USER '$(AttesterReaderUsernameCoston)'@'%' IDENTIFIED BY '$(AttesterReaderPasswordCoston)';
 
-ALTER USER '$(AttesterWriterUsernameSongbird)'@'localhost' IDENTIFIED BY '$(AttesterWriterPasswordSongbird)';
+ALTER USER '$(AttesterWriterUsernameSongbird)'@'$(DatabaseWriteAccessSource)' IDENTIFIED BY '$(AttesterWriterPasswordSongbird)';
 
 ALTER USER '$(AttesterReaderUsernameSongbird)'@'%' IDENTIFIED BY '$(AttesterReaderPasswordSongbird)';
 

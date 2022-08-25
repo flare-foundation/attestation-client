918cbd454c3c1d2d918048acc0bcbdb1cd5ab3c8
evi1m3 <david.pangerl@flare.network>
2022-08-25 12:06:39 +0200

    New MCC and connected fixes

--- test/indexer/blockValidityCheck.test.ts
+++ test/indexer/blockValidityCheck.test.ts
@@ -1,4 +1,4 @@
-// yarn test test/indexer/act-2.test.ts
+// yarn test test/indexer/blockValidityCheck.test.ts
 
 import { BlockBase, ChainType, IBlock, IXrpGetBlockRes, MCC, traceManager } from "@flarenetwork/mcc";
 import { XRPImplementation } from "@flarenetwork/mcc/dist/src/chain-clients/XrpRpcImplementation";
@@ -31,6 +31,13 @@ class MockXRPImplementation extends XRPImplementation {
 }
 
 class MockXrpBlock extends BlockBase<IXrpGetBlockRes> {
+    
+    get previousBlockHash(): string {
+        throw new Error("Method not implemented.");
+    }
+    get stdPreviousBlockHash(): string {
+        throw new Error("Method not implemented.");
+    }
 
     private block: IBlock;
 

b9019e966d371f7ed05300a847106d2c29553cdd
AvbrehtLuka <luka@flare.network>
2022-09-06 13:38:01 +0200

    minor random element updates

--- test/verification/attestationTypeHelpers.test.ts
+++ test/verification/attestationTypeHelpers.test.ts
@@ -1,9 +1,9 @@
 import { expect } from "chai";
-import { numberLikeToNumber } from "../../lib/verification/attestation-types/attestation-types-helpers";
+import { numberLikeToNumber, randomListElement } from "../../lib/verification/attestation-types/attestation-types-helpers";
 
 describe(`Attestation Helpers Tests`, function () {
-  describe(`number like to number`, function () {
-    it("string to number", function () {
+  describe(`numberLikeToNumber`, function () {
+    it("string to number safe", function () {
       const num = "9007199254740991";
       const convertedNumber = numberLikeToNumber(num);
 
@@ -11,13 +11,23 @@ describe(`Attestation Helpers Tests`, function () {
       expect(Number.isSafeInteger(convertedNumber)).to.eq(true);
     });
 
-    it("string to number", function () {
+    it("string to number unsafe", function () {
       const num = "9007199254740992";
       const convertedNumber = numberLikeToNumber(num);
 
       expect(convertedNumber).to.eq(Number.MAX_SAFE_INTEGER + 1);
       expect(convertedNumber).to.not.eq(Number.MAX_SAFE_INTEGER);
       expect(Number.isSafeInteger(convertedNumber)).to.eq(false);
+    });
+  });
+
+  describe(`randomListElement`, function () {
+    it("empty array", function () {
+      const arr: number[] = []
+      const randomElem = randomListElement(arr);
+
+      expect(randomElem).to.eq(undefined);
+      expect(typeof randomElem).to.eq('undefined')
     });
   });
 });

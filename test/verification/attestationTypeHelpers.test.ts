1c8f0b7045cce0a4506d9330fe9bb7401ded0b56
AvbrehtLuka <luka@flare.network>
2022-09-06 13:30:11 +0200

    added number processing test

---
+++ test/verification/attestationTypeHelpers.test.ts
@@ -0,0 +1,21 @@
+import { expect } from "chai";
+import { numberLikeToNumber } from "../../lib/verification/attestation-types/attestation-types-helpers";
+
+describe(`Attestation Helpers Tests`, function () {
+  describe(`number like to number`, function () {
+    it("string to number", function () {
+      const num = "9007199254740991";
+      const convertedNumber = numberLikeToNumber(num);
+
+      expect(convertedNumber).to.eq(Number.MAX_SAFE_INTEGER);
+    });
+
+    it("string to number", function () {
+      const num = "9007199254740992";
+      const convertedNumber = numberLikeToNumber(num);
+
+      expect(convertedNumber).to.eq(Number.MAX_SAFE_INTEGER + 1);
+      expect(convertedNumber).to.not.eq(Number.MAX_SAFE_INTEGER);
+    });
+  });
+});

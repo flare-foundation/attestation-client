f1540213f8fcb6f8f9d095d336070d9ff48c832e
evi1m3 <david.pangerl@flare.network>
2022-09-26 14:03:13 +0200

    Installer fix

--- scripts/install-certbot.sh
+++ scripts/install-certbot.sh
@@ -7,5 +7,6 @@ source .config.secret.sh2
 echo -e "${GREENBOLD}Installing CertBot${NC}"
 
 sudo ln -s /snap/bin/certbot /usr/bin/certbot
-sudo snap install --classic certbot
+#sudo snap install --classic certbot
+sudo apt install certbot -y
 sudo certbot certonly --standalone -d $HOSTNAME -m $CERT_EMAIL --agree-tos

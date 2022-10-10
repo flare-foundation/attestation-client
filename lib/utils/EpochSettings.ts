adcd42ed1712335e4646eb641da3fb9ff4d33813
grepod <gregor.podlogar@aflabs.si>
2022-10-10 14:19:03 +0200

    DocUpdate for the EpochSettings

--- lib/utils/EpochSettings.ts
+++ lib/utils/EpochSettings.ts
@@ -2,6 +2,13 @@ import BN from "bn.js";
 import { toBN } from "@flarenetwork/mcc";
 import { getTimeSec } from "./internetTime";
 
+// The word epoche in never mentioned in https://docs.flare.network/tech/state-connector/#procedure-overview
+// Each round consists of three phases: Request, Commit, Reveal. The phases run in consecutive epoches.
+// In each epoch with id n run a Request phase of round n, Commit phase of the round n-1 and Reveal phase of the round n-2
+
+/**
+ * Class for storing the settings of epochs
+ */
 export class EpochSettings {
   private _firstEpochStartTime: BN; // in seconds
   private _epochPeriod: BN; // in seconds
@@ -23,6 +30,9 @@ export class EpochSettings {
     return this._firstEpochId.add(diff.div(this._epochPeriod));
   }
 
+  /**
+   * Gets the id of the current epoch. It is the same as the id of the round that is currently in the request phase   *
+   */
   getCurrentEpochId(): BN {
     return this.getEpochIdForTime(toBN(getTimeSec() * 1000));
   }
@@ -38,23 +48,31 @@ export class EpochSettings {
   //   return this.getEpochTimeStart().add(this._epochPeriod);
   // }
 
-  // in milliseconds
-  getRoundIdTimeStartMs(id: BN | number): number {
-    return this._firstEpochStartTime.add(toBN(id).mul(this._epochPeriod)).toNumber(); // + this._epochPeriod.toNumber();
+  /**
+   * Gets the start time of the round in milliseconds. The round starts in the request phase.
+   */
+  getRoundIdTimeStartMs(round: BN | number): number {
+    return this._firstEpochStartTime.add(toBN(round).mul(this._epochPeriod)).toNumber(); // + this._epochPeriod.toNumber();
   }
 
-  // in milliseconds
-  getEpochIdTimeEndMs(id: BN | number): number {
-    return this.getRoundIdTimeStartMs(id) + this._epochPeriod.toNumber();
+  /**
+   * Gets the end time of the epoch in milliseconds
+   */
+  getEpochIdTimeEndMs(epochid: BN | number): number {
+    return this.getRoundIdTimeStartMs(epochid) + this._epochPeriod.toNumber();
   }
 
-  // in milliseconds
-  getRoundIdCommitTimeStartMs(id: BN | number): number {
-    return this.getRoundIdTimeStartMs(id) + this._epochPeriod.toNumber();
+  /**
+   * Gets the start time of the commit phase of the round in milliseconds
+   */
+  getRoundIdCommitTimeStartMs(round: BN | number): number {
+    return this.getRoundIdTimeStartMs(round) + this._epochPeriod.toNumber();
   }
 
-  // in milliseconds
-  getRoundIdRevealTimeStartMs(id: number): number {
-    return this.getRoundIdCommitTimeStartMs(id) + this._epochPeriod.toNumber();
+  /**
+   * Gets the start time of the Reveal phase of the round in milliseconds
+   */
+  getRoundIdRevealTimeStartMs(round: number): number {
+    return this.getRoundIdCommitTimeStartMs(round) + this._epochPeriod.toNumber();
   }
 }

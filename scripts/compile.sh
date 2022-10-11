afca69bb4925102e7e335e7e4a11af415ef3b6a1
evi1m3 <david.pangerl@flare.network>
2022-10-11 09:55:58 +0200

    Installer fix

--- scripts/compile.sh
+++ scripts/compile.sh
@@ -2,23 +2,8 @@ export CURRENT_DIR=$(pwd)
 
 yarn
 
-# first yarn calls sometimes fails
-yarn
-
 # hardhat
 yarn c
 
-# build MCC
-#bash ./script/replace-mcc-link.sh
-#cd ../multi-chain-client
-#yarn
-#yarn build
-
-#yarn link
-
-cd $CURRENT_DIR
-
-# build
-# yarn link flare-mcc
-
+# main build
 yarn build

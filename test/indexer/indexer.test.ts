7fa04615c7e271b3b81c4565e025474ae0c4a0b3
grepod <gregor.podlogar@aflabs.si>
2022-11-03 12:18:18 +0100

    indexer tests

--- test/indexer/indexer.test.ts
+++ test/indexer/indexer.test.ts
@@ -6,12 +6,14 @@ import { ChainConfiguration } from "../../lib/chain/ChainConfiguration";
 import { DBTransactionXRP0, DBTransactionXRP1 } from "../../lib/entity/indexer/dbTransaction";
 import { CachedMccClient } from "../../lib/caching/CachedMccClient";
 import { MockMccClient } from "../../lib/caching/test-utils/MockMccClient";
+import { IndexerConfiguration } from "../../lib/indexer/IndexerConfiguration";
 
 describe("Indexer", () => {
   let indexer = new Indexer(null, null, null, null);
   indexer.chainType = ChainType.XRP;
   indexer.chainConfig = new ChainConfiguration();
   indexer.chainConfig.name = "XRP";
+  indexer.config = new IndexerConfiguration();
   indexer.prepareTables();
   const mockMccClient = new MockMccClient();
   indexer.cachedClient = new CachedMccClient(ChainType.XRP, { forcedClient: mockMccClient });
@@ -33,5 +35,23 @@ describe("Indexer", () => {
   it("Should getBlockFromClientbyHash mock", async () => {
     let block = await indexer.getBlockFromClientByHash("nekej", "0F12C5AA5B4334E67FD2BA9BD407A39C74C483C7D3CFA0218A3C9C83B59374F8");
     expect(block.blockHash).to.be.equal("0F12C5AA5B4334E67FD2BA9BD407A39C74C483C7D3CFA0218A3C9C83B59374F8");
+  });
+
+  it("Should get syncTimeDays", () => {
+    expect(indexer.syncTimeDays()).to.be.eq(2);
+    indexer.chainConfig.syncTimeDays = 3;
+    expect(indexer.syncTimeDays()).to.be.eq(3);
+  });
+
+  it("Should getStateEntry", () => {
+    const state = indexer.getStateEntry("something", 42);
+    expect(state.name).to.be.eq("XRP_something");
+    expect(state.valueNumber).to.be.eq(42);
+  });
+
+  it("Should getStateEntryString", () => {
+    const state = indexer.getStateEntryString("something", "something else", 42);
+    expect(state.name).to.be.eq("XRP_something");
+    expect(state.valueString).to.be.eq("something else");
   });
 });

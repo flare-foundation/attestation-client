7b6d646bca81eefe550802ae1c92237cd96011de
grepod <gregor.podlogar@aflabs.si>
2022-11-24 15:57:23 +0100

    remarks + tests added

---
+++ remarks.md
@@ -0,0 +1,3 @@
+BaseEntity.ts, dbTransaction.ts : Why we do not use transactionId as the primaryColumn??
+
+MerkleTree.ts : verify has to be independent of merkle tree
--- test/indexer/chain-collector-helper.test.ts
+++ test/indexer/chain-collector-helper.test.ts
@@ -1,4 +1,13 @@
-import { IXrpGetBlockRes, IXrpGetTransactionRes, UtxoBlock, UtxoTransaction, XrpBlock, XrpTransaction, xrp_ensure_data } from "@flarenetwork/mcc";
+import {
+  IXrpGetBlockRes,
+  IXrpGetTransactionRes,
+  UtxoBlock,
+  UtxoMccCreate,
+  UtxoTransaction,
+  XrpBlock,
+  XrpTransaction,
+  xrp_ensure_data,
+} from "@flarenetwork/mcc";
 import { DBBlockBTC } from "../../lib/entity/indexer/dbBlock";
 import { augmentBlock } from "../../lib/indexer/chain-collector-helpers/augmentBlock";
 import { ChainType } from "@flarenetwork/mcc";
@@ -7,8 +16,16 @@ import { augmentTransactionUtxo, augmentTransactionXrp } from "../../lib/indexer
 import { DBTransactionBTC0 } from "../../lib/entity/indexer/dbTransaction";
 import * as resBTCBlock from "../mockData/BTCBlock.json";
 import * as resBTCTx from "../mockData/BTCTx.json";
+import * as resBTCTxAlt from "../mockData/BTCTxAlt.json";
 import * as resXRPBlock from "../mockData/XRPBlock.json";
 import * as resXRPTx from "../mockData/XRPTx.json";
+import { CachedMccClient, CachedMccClientOptionsFull } from "../../lib/caching/CachedMccClient";
+import { Interlacing } from "../../lib/indexer/interlacing";
+import { globalTestLogger } from "../../lib/utils/logger";
+import { DatabaseService, DatabaseSourceOptions } from "../../lib/utils/databaseService";
+import { LimitingProcessor } from "../../lib/caching/LimitingProcessor";
+import { UtxoBlockProcessor } from "../../lib/indexer/chain-collector-helpers/blockProcessor";
+import { getFullTransactionUtxo } from "../../lib/indexer/chain-collector-helpers/readTransaction";
 
 describe("augmentBlock", () => {
   it("Should create entity for a block", async () => {
@@ -45,3 +62,64 @@ describe("augmentTransaction", () => {
     // });
   });
 });
+
+describe("readTransaction", () => {
+  const BtcMccConnection = {
+    url: process.env.BTC_URL,
+    username: process.env.BTC_USERNAME,
+    password: process.env.BTC_PASSWORD,
+  } as UtxoMccCreate;
+
+  let cachedMccClientOptionsFull: CachedMccClientOptionsFull = {
+    transactionCacheSize: 2,
+    blockCacheSize: 2,
+    cleanupChunkSize: 2,
+    activeLimit: 1,
+    clientConfig: BtcMccConnection,
+  };
+
+  const databaseConnectOptions = new DatabaseSourceOptions();
+  databaseConnectOptions.database = process.env.DATABASE_NAME1;
+  databaseConnectOptions.username = process.env.DATABASE_USERNAME;
+  databaseConnectOptions.password = process.env.DATBASE_PASS;
+  const dataService = new DatabaseService(globalTestLogger, databaseConnectOptions);
+  const cachedClient = new CachedMccClient(ChainType.BTC, cachedMccClientOptionsFull);
+  const interlacing = new Interlacing();
+  let utxoBlockProcessor: UtxoBlockProcessor;
+  const tx = new UtxoTransaction(resBTCTxAlt);
+  before(async () => {
+    await interlacing.initialize(globalTestLogger, dataService, ChainType.BTC, 3600, 12);
+    utxoBlockProcessor = new UtxoBlockProcessor(interlacing, cachedClient);
+  });
+
+  // it("should get full transaction utxo", async (done) => {
+  //   console.log(tx.reference);
+  //   const fullTx = await getFullTransactionUtxo(cachedClient, tx, utxoBlockProcessor);
+  //   console.log(fullTx);
+  // });
+});
+// describe("UtxoBlockProcessor", () => {
+//   const BtcMccConnection = {
+//     url: "https://bitcoin-api.flare.network",
+//     username: "public",
+//     password: "d681co1pe2l3wcj9adrm2orlk0j5r5gr3wghgxt58tvge594co0k1ciljxq9glei",
+//   } as UtxoMccCreate;
+
+//   let cachedMccClientOptionsFull: CachedMccClientOptionsFull = {
+//     transactionCacheSize: 2,
+//     blockCacheSize: 2,
+//     cleanupChunkSize: 2,
+//     activeLimit: 1,
+//     clientConfig: BtcMccConnection,
+//   };
+
+//   const databaseConnectOptions = new DatabaseSourceOptions();
+//   databaseConnectOptions.database = "AttDBtestLimit";
+//   databaseConnectOptions.username = "root";
+//   databaseConnectOptions.password = "praporscak";
+//   const dataService = new DatabaseService(globalTestLogger, databaseConnectOptions);
+//   const cachedClient = new CachedMccClient(ChainType.BTC, cachedMccClientOptionsFull);
+//   const interlacing = new Interlacing();
+//   let utxoBlockProcessor: UtxoBlockProcessor;
+//
+// });

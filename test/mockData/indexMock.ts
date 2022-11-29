d55a0396ea2415a1fe2765d2b332a4d15543620f
grepod <gregor.podlogar@aflabs.si>
2022-11-29 14:59:55 +0100

    tests added

--- test/mockData/indexMock.ts
+++ test/mockData/indexMock.ts
@@ -5,11 +5,16 @@ import * as resBTCBlock from "../mockData/BTCBlock.json";
 import * as resBTCTx from "../mockData/BTCTx.json";
 import * as resBTCTxAlt from "../mockData/BTCTxAlt.json";
 import * as resBTCBlockAlt from "../mockData/BTCBlockAlt.json";
+import { augmentBlock } from "../../lib/indexer/chain-collector-helpers/augmentBlock";
+import { DBBlockBTC } from "../../lib/entity/indexer/dbBlock";
 
 export const TestBlockBTC = new UtxoBlock(resBTCBlock);
 export const TestTxBTC = new UtxoTransaction(resBTCTx);
 export const TestBlockBTCAlt = new UtxoBlock(resBTCBlockAlt as unknown as IUtxoGetBlockRes);
 export const TestTxBTCAlt = new UtxoTransaction(resBTCTxAlt);
+
+export const AugTestBlockBTC = augmentBlock(DBBlockBTC, TestBlockBTC);
+export const AugTestBlockBTCAlt = augmentBlock(DBBlockBTC, TestBlockBTCAlt);
 
 const waitTx = async (tx) => {
   return tx;

f514b6c90b87f458839e125236c1bf4ef2a78edc
alenabelium <33514602+alenabelium@users.noreply.github.com>
2022-12-01 12:35:02 +0100

    attestation type

--- lib/servers/ws-server/src/controllers/verifier.controller.ts
+++ lib/servers/ws-server/src/controllers/verifier.controller.ts
@@ -1,5 +1,6 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
+import { AttestationRequest } from '../../../../verification/attestation-types/attestation-types';
 import { ApiResponse, handleApiResponse } from '../../../common/src';
 import { XRPProcessorService } from '../services/xrp-processor';
 
@@ -10,9 +11,9 @@ export class VerifierController {
   constructor(private processor: XRPProcessorService) { }
 
   @Post("")
-  public async processAttestationRequest(@Body() attestationRequest: any): Promise<ApiResponse<any>> {
+  public async processAttestationRequest(@Body() attestationRequest: AttestationRequest): Promise<ApiResponse<any>> {
     return handleApiResponse(
-      this.processor.verify(attestationRequest.data)
+      this.processor.verify(attestationRequest)
     )
   }
 

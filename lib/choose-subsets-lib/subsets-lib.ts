ce06bc32b6f52f874f97212a72a73095d5c4beaf
LukaAvbreht <l.avbreht@gmail.com>
2022-12-07 17:24:51 +0100

    subsets

--- lib/choose-subsets-lib/subsets-lib.ts
+++ lib/choose-subsets-lib/subsets-lib.ts
@@ -1,33 +1,28 @@
 const SUBGROUP_TO_BIG_ERROR = "Groups can ony have subgroups whose size is smaller or equal to original group's size";
 
-function chooseCandidate(votes: number[], groupSize: number) {
+export function countOnes(num: number){
+  let sum = 0;
+
+}
+
+export function chooseCandidate(votes: number[], groupSize: number) {
   if (votes.length < groupSize) {
     throw Error(SUBGROUP_TO_BIG_ERROR);
   }
-}
 
-export function getSubsetsOfSizeSlow(array: any[], size: number) {
-  if (array.length < size) {
-    throw Error(SUBGROUP_TO_BIG_ERROR);
+  const allSubsets = getSubsetsOfSize(votes, groupSize);
+
+  const candidates = []
+
+  for(const subset of allSubsets){
+    let candidate = subset[0]
+    for(const elem of subset){
+      candidate = candidate & elem
+    }
+    candidates.push(candidate)
   }
 
-  const result = [];
-
-  function helper(arr: any[], splitIndex: number) {
-    if (arr.length === size) {
-      result.push(arr);
-      return;
-    }
-    if (splitIndex + 1 > array.length) {
-      return;
-    }
-    // Old idea comment
-    helper(arr.concat(array[splitIndex]), splitIndex + 1);
-    helper(arr, splitIndex + 1);
-  }
-
-  helper([], 0);
-  return result;
+  return candidates
 }
 
 export function getSubsetsOfSize(array: any[], size: number) {

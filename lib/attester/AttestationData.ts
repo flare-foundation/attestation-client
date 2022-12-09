868a0313972351aab910e3849c38fdbb6ea9af5f
LukaAvbreht <l.avbreht@gmail.com>
2022-12-09 14:33:44 +0100

    choose lib implementation

--- lib/attester/AttestationData.ts
+++ lib/attester/AttestationData.ts
@@ -1,6 +1,5 @@
 import { toBN } from "@flarenetwork/mcc";
 import BN from "bn.js";
-import { AttestationChooseBytes } from "../../typechain-truffle/StateConnector";
 import { getAttestationTypeAndSource } from "../verification/generated/attestation-request-parse";
 import { AttestationType } from "../verification/generated/attestation-types-enum";
 import { SourceId } from "../verification/sources/sources";

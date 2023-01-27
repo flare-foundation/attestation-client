bfc0b575c9ec534bcc30e1ba9b43188335517670
LukaAvbreht <l.avbreht@gmail.com>
2023-01-27 13:57:37 +0100

    updated verifier endpoint

--- src/servers/verifier-server/src/controllers/verifier.controller.ts
+++ src/servers/verifier-server/src/controllers/verifier.controller.ts
@@ -1,31 +1,36 @@
-import { Body, Controller, Inject, Post, UseGuards } from '@nestjs/common';
-import { ApiSecurity, ApiTags } from '@nestjs/swagger';
-import { AttestationRequest, PrepareRequest } from '../../../../verification/attestation-types/attestation-types';
-import { ApiResponse, handleApiResponse } from '../../../common/src';
+import { Body, Controller, Inject, Post, UseGuards } from "@nestjs/common";
+import { ApiSecurity, ApiTags } from "@nestjs/swagger";
+import { AttestationRequest } from "../../../../verification/attestation-types/attestation-types";
+import { ApiResponse, handleApiResponse } from "../../../common/src";
 // import { AuthGuard } from '../guards/auth.guard';
-import { AuthGuard } from '@nestjs/passport';
-import { VerifierProcessor } from '../services/verifier-processors/verifier-processor';
+import { AuthGuard } from "@nestjs/passport";
+import { VerifierProcessor } from "../services/verifier-processors/verifier-processor";
+import { ARType } from "../../../../verification/generated/attestation-request-types";
 
-@ApiTags('Verifier')
+@ApiTags("Verifier")
 @Controller("query")
-@UseGuards(AuthGuard('api-key'))
-@ApiSecurity('X-API-KEY')
+@UseGuards(AuthGuard("api-key"))
+@ApiSecurity("X-API-KEY")
 export class VerifierController {
-
-  constructor(@Inject("VERIFIER_PROCESSOR") private processor: VerifierProcessor) { }
+  constructor(@Inject("VERIFIER_PROCESSOR") private processor: VerifierProcessor) {}
 
   @Post("")
   public async processAttestationRequest(@Body() attestationRequest: AttestationRequest): Promise<ApiResponse<any>> {
-    return handleApiResponse(
-      this.processor.verify(attestationRequest)
-    )
+    return handleApiResponse(this.processor.verify(attestationRequest));
   }
 
   @Post("prepare")
-  public async check(@Body() request: PrepareRequest): Promise<ApiResponse<any>> {
-    return handleApiResponse(
-      this.processor.prepareRequest(request)
-    )
+  public async check(@Body() request: ARType): Promise<ApiResponse<any>> {
+    return handleApiResponse(this.processor.prepareRequest(request));
   }
 
+  @Post("integrity")
+  public async getIntegrity(@Body() request: ARType): Promise<ApiResponse<string>> {
+    return handleApiResponse(this.processor.getMessageIntegrityCheck(request));
+  }
+
+  @Post("prepareAttestation")
+  public async getAttestationData(@Body() request: ARType): Promise<ApiResponse<string>> {
+    return handleApiResponse(this.processor.getAttestationData(request));
+  }
 }

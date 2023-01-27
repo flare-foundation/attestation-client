43b6cbf68eb46bfc7afbd9bb577a0efe6bee6e09
LukaAvbreht <l.avbreht@gmail.com>
2023-01-27 13:57:37 +0100

    updated verifier endpoint

--- src/servers/verifier-server/src/services/verifier-processors/verifier-processor.ts
+++ src/servers/verifier-server/src/services/verifier-processors/verifier-processor.ts
@@ -1,16 +1,31 @@
-import { AttestationRequest, PrepareRequest, Verification } from '../../../../../verification/attestation-types/attestation-types';
-import { encodeRequest } from '../../../../../verification/generated/attestation-request-encode';
-import { getAttestationTypeAndSource } from '../../../../../verification/generated/attestation-request-parse';
-import { getAttestationTypeName } from '../../../../../verification/generated/attestation-types-enum';
-import { getSourceName } from '../../../../../verification/sources/sources';
+import { AttestationRequest, MIC_SALT, Verification } from "../../../../../verification/attestation-types/attestation-types";
+import { hashPayment } from "../../../../../verification/generated/attestation-hash-utils";
+import { encodeRequest } from "../../../../../verification/generated/attestation-request-encode";
+import { getAttestationTypeAndSource } from "../../../../../verification/generated/attestation-request-parse";
+import { ARType } from "../../../../../verification/generated/attestation-request-types";
+import { getAttestationTypeName } from "../../../../../verification/generated/attestation-types-enum";
+import { getSourceName } from "../../../../../verification/sources/sources";
 
 export abstract class VerifierProcessor {
   public abstract verify(attestationRequest: AttestationRequest): Promise<Verification<any, any>>;
   public abstract supportedAttestationTypes(): string[];
   public abstract supportedSource(): string;
-  public prepareRequest(request: PrepareRequest): Promise<Verification<any, any>> {
-    return this.verify({ request: encodeRequest(request.request) })
-  };
+  public prepareRequest(request: ARType): Promise<Verification<any, any>> {
+    return this.verify({ request: encodeRequest(request) });
+  }
+
+  public async getMessageIntegrityCheck(request: ARType): Promise<string> {
+    const data = await this.verify({ request: encodeRequest(request) });
+    const integrity = hashPayment(data.request, data.response, MIC_SALT);
+    return integrity;
+  }
+
+  public async getAttestationData(request: ARType): Promise<string> {
+    const data = await this.verify({ request: encodeRequest(request) });
+    const integrity = hashPayment(data.request, data.response, MIC_SALT);
+    request.messageIntegrityCode = integrity;
+    return encodeRequest(request);
+  }
 
   public assertIsSupported(attestationRequest: AttestationRequest) {
     if (process.env.NODE_ENV === "development" && process.env.TEST_IGNORE_SUPPORTED_ATTESTATION_CHECK_TEST) {
@@ -22,8 +37,11 @@ export abstract class VerifierProcessor {
     }
     let attTypeName = getAttestationTypeName(attestationType);
     if (this.supportedAttestationTypes().indexOf(attTypeName) < 0) {
-      throw new Error(`Unsupported attestation type '${attTypeName}'. Supported types for verifier '${this.supportedSource()}': ${this.supportedAttestationTypes().join(", ")}`);
+      throw new Error(
+        `Unsupported attestation type '${attTypeName}'. Supported types for verifier '${this.supportedSource()}': ${this.supportedAttestationTypes().join(
+          ", "
+        )}`
+      );
     }
   }
-
 }

1c680df934122eb81cac7e1832d67d3f4b4b9fe8
LukaAvbreht <l.avbreht@gmail.com>
2023-01-27 16:19:18 +0100

    updated get specific vote method

--- src/servers/web-server/src/controllers/proof.controller.ts
+++ src/servers/web-server/src/controllers/proof.controller.ts
@@ -1,21 +1,23 @@
-import { Controller, Get, Param, ParseIntPipe } from '@nestjs/common';
-import { SystemStatus } from '../dtos/SystemStatus.dto';
-import { VotingRoundRequest } from '../dtos/VotingRoundRequest.dto';
-import { VotingRoundResult } from '../dtos/VotingRoundResult.dto';
-import { ProofEngineService } from '../services/proof-engine.service';
-import { ApiTags } from '@nestjs/swagger';
-import { ApiResponse, handleApiResponse } from '../../../common/src';
+import { Body, Controller, Get, Param, ParseIntPipe, Post } from "@nestjs/common";
+import { SystemStatus } from "../dtos/SystemStatus.dto";
+import { VotingRoundRequest } from "../dtos/VotingRoundRequest.dto";
+import { VotingRoundResult } from "../dtos/VotingRoundResult.dto";
+import { ProofEngineService } from "../services/proof-engine.service";
+import { ApiTags } from "@nestjs/swagger";
+import { ApiResponse, handleApiResponse } from "../../../common/src";
 
-@ApiTags('Proof')
+export interface specificProofRequest {
+  roundId: number;
+  callData: string;
+}
+
+@ApiTags("Proof")
 @Controller("api/proof")
 export class ProofController {
-
-  constructor(private proofEngine: ProofEngineService) { }
+  constructor(private proofEngine: ProofEngineService) {}
 
   @Get("votes-for-round/:roundId")
-  public async votesForRound(
-    @Param('roundId', new ParseIntPipe()) roundId: number
-  ): Promise<ApiResponse<VotingRoundResult[]>> {
+  public async votesForRound(@Param("roundId", new ParseIntPipe()) roundId: number): Promise<ApiResponse<VotingRoundResult[]>> {
     try {
       let result = await this.proofEngine.getProofForRound(roundId);
       if (result) {
@@ -27,10 +29,21 @@ export class ProofController {
     }
   }
 
+  @Post("getSpecificProof")
+  public async getSpecificProofController(@Body() roundRequest: specificProofRequest): Promise<ApiResponse<VotingRoundResult>> {
+    try {
+      let result = await this.proofEngine.getSpecificProofForRound(roundRequest.roundId, roundRequest.callData);
+      if (result) {
+        return new ApiResponse<VotingRoundResult>(result);
+      }
+      return new ApiResponse<VotingRoundResult>(null, "PENDING");
+    } catch (reason: any) {
+      throw new ApiResponse<VotingRoundResult>(undefined as any, "ERROR", "" + reason, reason);
+    }
+  }
+
   @Get("requests-for-round/:roundId")
-  public async requestsForRound(
-    @Param('roundId', new ParseIntPipe()) roundId: number
-  ): Promise<ApiResponse<VotingRoundRequest[]>> {
+  public async requestsForRound(@Param("roundId", new ParseIntPipe()) roundId: number): Promise<ApiResponse<VotingRoundRequest[]>> {
     try {
       let result = await this.proofEngine.getRequestsForRound(roundId);
       if (result) {

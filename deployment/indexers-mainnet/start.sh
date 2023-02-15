1e48f18c52a5a45989454ef2843bd2103e8e9225
alenabelium <33514602+alenabelium@users.noreply.github.com>
2023-02-15 16:33:05 +0100

    config fixes

--- deployment/indexers-mainnet/start.sh
+++ deployment/indexers-mainnet/start.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
 chain="$1"
+spammer="$2"
 
-docker-compose -f docker-compose-indexer-$chain.yaml -p indexer-$chain up -d
+if [[ $spammer == spammer ]] ; then
+   docker-compose -f docker-compose-indexer-$chain.yaml -p indexer-$chain --profile spammer up -d 
+else
+   docker-compose -f docker-compose-indexer-$chain.yaml -p indexer-$chain up -d
+fi
--- deployment/indexers-mainnet/stop.sh
+++ deployment/indexers-mainnet/stop.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
 chain="$1"
+spammer="$2"
 
-docker-compose -f docker-compose-indexer-$chain.yaml -p indexer-$chain down
+if [[ $spammer == spammer ]] ; then
+   docker-compose -f docker-compose-indexer-$chain.yaml -p indexer-$chain stop spammer 
+else
+   docker-compose -f docker-compose-indexer-$chain.yaml -p indexer-$chain down
+fi

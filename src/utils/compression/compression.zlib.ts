dc05214dec2a821a46dd7d9910625392c2d82e16
david.pangerl <david.pangerl@flare.network>
2023-06-09 12:14:12 +0000

    Indexer save optimization

---
+++ src/utils/compression/compression.zlib.ts
@@ -0,0 +1,52 @@
+const zlib = require('zlib');
+
+//const ZstdCodec = require('zstd-codec').ZstdCodec;
+
+// tested:
+// - deflate - default (1st tested)
+// - [gzip] - ~10% better than deflate (10MB/sec) same speed as deflate
+// - brotli - slow (0.5MB/sec), but very good compression (better than the rest)
+// - zpack - input/output is object (not string) but in general very good for tables but bit usefull in our case
+
+// todo:
+// - zstd - should be 20% better than gzip and faster
+
+/**
+ * Compress string data into base64 string.
+ * @param data 
+ * @returns compressed data as base64 string
+ */
+export function compress(data: string): string {
+    return zlib.deflateSync(data, {level: 9}).toString('base64');
+}
+
+/**
+ * Decompress base64 string compressed with @Compress function back into source string.
+ * @param compressedData base64 string compressed with @Compress function
+ * @returns decompressed data as string
+ */
+export function decompress(compressedData: string): string {
+    return zlib.inflateSync(new Buffer(compressedData, 'base64')).toString();
+}
+
+/**
+ * Compress string data into binary buffer.
+ * @param data 
+ * @returns compressed data as binary buffer
+ */
+export function compressBin(data: string): Buffer {
+    //return zlib.deflateSync(data, {level: 9});
+    return zlib.gzipSync(data, {level: 9});
+    //return zlib.brotliCompressSync(data);
+}
+
+/**
+ * Decompress binary buffer compressed with @CompressBin function back into source string.
+ * @param compressedDataBuffer binary data compressed with @CompressBin function
+ * @returns decompressed data string
+ */
+export function decompressBin(compressedDataBuffer: Buffer): string {
+    //return zlib.inflateSync(compressedDataBuffer).toString();
+    return zlib.gunzipSync(compressedDataBuffer).toString();
+    //return zlib.brotliDecompressSync(compressedDataBuffer).toString();
+}   

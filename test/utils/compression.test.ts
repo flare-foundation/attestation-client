5461c2781adc8b46a56896a976f99425cf9b5929
alenabelium <33514602+alenabelium@users.noreply.github.com>
2023-06-14 11:37:00 +0200

    removing unecessary dependencies

--- test/utils/compression.test.ts
+++ test/utils/compression.test.ts
@@ -6,9 +6,9 @@ import { initializeTestGlobalLogger } from "../../src/utils/logging/logger";
 import { compress, compressBin, decompress, decompressBin } from "../../src/utils/compression/compression.zlib";
 import { getTestFile } from "../test-utils/test-utils";
 
-import zstd from "@foxglove/wasm-zstd";
-import { TextEncoder } from "util";
-import { TextDecoder } from "util";
+// import zstd from "@foxglove/wasm-zstd";
+// import { TextEncoder } from "util";
+// import { TextDecoder } from "util";
 
 const compressionTextData = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
 const compressionJsonData = `[

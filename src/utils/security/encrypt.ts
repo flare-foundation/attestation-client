98eef3b9afbcec07b493af0d664b551c9d8704df
grePod <gregor.podlogar@aflabs.si>
2024-01-17 14:50:09 +0100

    encrypt string update, typos

--- src/utils/security/encrypt.ts
+++ src/utils/security/encrypt.ts
@@ -7,7 +7,8 @@ import crypto from "crypto";
  * @returns
  */
 export function encryptString(password: string, text: string): string {
-  const passwordHash = crypto.createHash("sha256").update(password, "ascii").digest();
+  // const passwordHash = crypto.createHash("sha256").update(password, "ascii").digest();
+  const passwordHash = crypto.scryptSync(password, "Flare", 32, { N: 65536, maxmem: 134217728 });
   const initVector = crypto.randomBytes(16);
   const cipher = crypto.createCipheriv("aes-256-gcm", passwordHash, initVector);
   const encbuf = cipher.update(text, "utf-8");
@@ -21,7 +22,9 @@ export function encryptString(password: string, text: string): string {
  * @returns
  */
 export function decryptString(password: string, encryptedText: string): string {
-  const passwordHash = crypto.createHash("sha256").update(password, "ascii").digest();
+  // const passwordHash = crypto.createHash("sha256").update(password, "ascii").digest();
+  const passwordHash = crypto.scryptSync(password, "Flare", 32, { N: 65536, maxmem: 134217728 });
+
   const encIvBuf = Buffer.from(encryptedText, "base64");
   const initVector = encIvBuf.subarray(0, 16);
   const encBuf = encIvBuf.subarray(16);
